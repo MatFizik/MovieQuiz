3c769faf6e804dd1caff4a8368eea454f6502b26
Adilkhan <dfw.adilhan@gmail.com>
2026-04-13 15:54:18 +0600

    логика ответов на вопрос и показ результата

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -11,15 +11,67 @@ final class MovieQuizViewController: UIViewController {
     
     @IBOutlet private weak var previewImage: UIImageView!
     
+    private var currentQuestionIndex: Int = 0
+    private var currentQuestion = QuizQuestion(image: "", text: "", correctAnswer: true)
+    
+    private var correctAnswers: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        currentQuestion = questions[currentQuestionIndex]
+        let quizStep = convert(model: currentQuestion)
+    
+        show(quiz: quizStep)
     }
     
     @IBAction private func noButtonClicked(_ sender: Any) {
+        if currentQuestion.correctAnswer {
+            showAnswerResult(isCorrect: false)
+        }
     }
     
     @IBAction private func yesButtonClicked(_ sender: Any) {
+        if currentQuestion.correctAnswer {
+            showAnswerResult(isCorrect: true)
+        }
+    }
+    
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        return QuizStepViewModel(
+            image: UIImage(named: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)"
+        )
+    }
+    
+    private func show(quiz step: QuizStepViewModel){
+        previewImage.image = step.image
+        questionLabel.text = step.question
+        indexLabel.text = step.questionNumber
+    }
+    
+    private func showAnswerResult(isCorrect: Bool){
+        previewImage.layer.borderWidth = 8
+        previewImage.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.showNextQiuestionOrResult()
+            self.previewImage.layer.borderColor = nil
+            self.previewImage.layer.borderWidth = 0.0
+        }
+        
+        
+    }
+    
+    private func showNextQiuestionOrResult(){
+        if currentQuestionIndex == questions.count - 1 {}
+        else {
+            currentQuestionIndex += 1
+            currentQuestion = questions[currentQuestionIndex]
+            let quizStep = convert(model: currentQuestion)
+            show(quiz: quizStep)
+        }
     }
 }
 
@@ -27,6 +79,12 @@ struct QuizQuestion{
     let image: String
     let text: String
     let correctAnswer: Bool
+}
+
+struct QuizStepViewModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
 }
 
 // массив mock-вопросов
